137f02c6914c7a3c07d8ad4688180e75fef85b2a
basat <itzbasatmaqsood@gmail.com>
2025-01-07 17:02:25 +0500

    V1.0 completed new languages added

--- src/app/components/CategoryLinks.tsx
+++ src/app/components/CategoryLinks.tsx
@@ -35,7 +35,7 @@ function CategoryLinks() {
           );
         })}
       </div>
-        <p className="text-center">Developed with <IconHeartFilled color="red" className="inline"/> By <a href="https://www.linkedin.com/in/basatmaqsood/">Basat Maqsood</a></p>
+        <p className="text-center">Developed with <IconHeartFilled color="red" className="inline"/> By <a href="https://www.linkedin.com/in/basatmaqsood/" target="_blank" className="no-underline">Basat Maqsood</a></p>
     </div>
   );
 }
