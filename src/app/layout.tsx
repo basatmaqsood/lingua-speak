278001fa0d53b2f52dcb2ebdfcc9d0b78e8846d0
basat <itzbasatmaqsood@gmail.com>
2025-01-08 09:11:14 +0500

    Optimized useTranslate hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       <script src="./node_modules/preline/dist/preline.js"></script>
 
       <body className={inter.className}>{children}</body>
